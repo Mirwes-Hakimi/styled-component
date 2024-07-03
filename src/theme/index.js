8415007c6740f8f3775f4a8d3048408c95700255
Mirwes Hakimi <mirwes215@gmail.com>
2024-07-02 19:36:24 -0700

    created the themes primaryColor to royalblue...

---
+++ src/theme/index.js
@@ -0,0 +1,8 @@
+export default {
+    primaryColor: 'royalblue',
+    secondaryColor: 'crimson',
+    tertiaryColor: 'mediumseagreen',
+    black: 'black',
+    white: 'white',
+    breakpointMobile: `(max-with: 550px)`,
+}
